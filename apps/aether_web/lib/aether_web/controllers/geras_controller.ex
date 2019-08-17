164d6a2dd04200c84e52cac65babcd062bf24478
Quinten Cisneros-Gruenthal <quintengruenthal@gmail.com>
2019-08-16 19:20:15 -0700

    Cleaning Up

--- apps/aether_web/lib/aether_web/controllers/geras_controller.ex
+++ apps/aether_web/lib/aether_web/controllers/geras_controller.ex
@@ -4,7 +4,7 @@ defmodule AetherWeb.GerasController do
   import Exexec
 
   def grades(conn, _params) do
-    render(conn, "grades.html", tests: "example")
+    render(conn, "grades.html", tests: "grade")
   end
 
   def example(conn, _params) do
@@ -18,35 +18,13 @@ defmodule AetherWeb.GerasController do
   def grade(conn, _params) do
     command = "cd apps/aether_web/assets/static/examples && momus"
     {:ok, pid, os_pid} = run_link(command, stdout: true)
-    #result = Enum.at(out[:stdout], 0)
-    #IO.puts(result)
-    #IO.puts(out[:stdout])
+
     critique = receive do
       {:stdout, ^os_pid, output} ->
         "{\"tests\": [\n" <> output <> "]}\n"
     end
 
-    #IO.puts(critique)
-    ant = """
-{\"tests\": [
-  {\"name\": \"t01\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t02\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t03\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t04\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t05\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t06\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t07\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t08\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t09\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t10\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t11\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t12\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t13\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t14\", \"passed\": true, \"value\": 1},
-  {\"name\": \"t15\", \"passed\": true, \"value\": 1}
-]}
-"""
-    case Jason.decode(ant) do
+    case Jason.decode(critique) do
       {:ok, critique} ->
         json(conn, critique)
       {:error, _} ->
